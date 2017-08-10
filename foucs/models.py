15e8e6bfd13c05d3e3f22794a82cc7bd87d77b94
zhumaobi <zhumaobi@qq.com>
2017-08-10 11:25:42 +0800

    重新定义了所有模型中的外键为User

--- foucs/models.py
+++ foucs/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class MyUser(models.Model):
 	user = models.OneToOneField(User)
-	profile = models.TextField('profile')
+	profile = models.TextField('profile', blank=True)
+	birthday = models.DateField('birthday', null=True)
 
 class Artical(models.Model):
 	title = models.CharField(max_length=256)
@@ -13,7 +14,7 @@ class Artical(models.Model):
 	poll_nums = models.IntegerField(default=0)
 	comment_nums = models.IntegerField(default=0)
 	keep_nums = models.IntegerField(default=0)
-	user = models.ManyToManyField('MyUser', blank=True)
+	user = models.ManyToManyField(User, blank=True)
 	pub_date = models.DateTimeField(auto_now_add=True)
 	latest_pub_date = models.DateTimeField(auto_now=True)
 	column = models.ForeignKey('Column')
@@ -28,24 +29,25 @@ class Column(models.Model):
 
 class Author(models.Model):
 	name = models.CharField(max_length=256)
-	email = models.EmailField('email')
-	password = models.CharField('password', max_length=256)
+	email = models.EmailField('email',null=True)
+	password = models.CharField('password', max_length=256, blank=True)
 	register_date = models.DateTimeField(auto_now_add=True)
-	profile = models.CharField('profile',max_length=256)
+	profile = models.CharField('profile',max_length=256, blank=True)
 
 	def __str__(self):
 		return self.name
 
 class Comment(models.Model):
 	comment_text = models.TextField('comment_text')
-	user = models.ForeignKey(MyUser)
+	user = models.ForeignKey(User)
 	artical = models.ForeignKey(Artical)
 	poll_nums = models.IntegerField(default=0)
+	post_nums = models.IntegerField(default=0)
 	pub_date = models.DateTimeField(auto_now_add=True)
 	def __str__(self):
 		return self.coment_text
 
 class Poll(models.Model):
-	user = models.ForeignKey(MyUser)
-	artical = models.ForeignKey(Artical)
-	comment = models.ForeignKey(Comment)
+	user = models.ForeignKey(User)
+	artical = models.ForeignKey(Artical, null=True)
+	comment = models.ForeignKey(Comment, null=True)
